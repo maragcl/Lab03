423c52eed959ea0efef28ef0de766b681d5673c7
Maria Aguilar <maria.aguilar.c@tecsup.edu.pe>
2021-03-24 20:26:24 -0500

    quinto commit

--- src/content.js
+++ src/content.js
@@ -2,18 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Part from './part'
 function Content(props) {
-    /*return <p>{props.parrafo}-{props.excersize}</p>;*/
-    const ejercicio1 = 10
-    const ejercicio2= 16
-    const ejercicio3=4
     return (
         <div>
-            <Part parrafo="Desarrollo de aplicaciones Web Avanzado" 
-            excersize= {ejercicio1}/>
-            <Part parrafo="Desarrollo de aplicaciones Web Avanzado" 
-            excersize= {ejercicio2}/>
-            <Part parrafo="Desarrollo de aplicaciones Web Avanzado" 
-            excersize= {ejercicio3}/>
+            <Part />
+
         </div>
     )
   }
--- src/part.js
+++ src/part.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 function Part(props) {
-    return <p>{props.parrafo}-{props.excersize}</p>;
+  const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
+    }
+    const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    const part3 = {
+      name: 'State of a component',
+      exercises: 14
+    }
+    return <p>{props.name}-{props.excersises}</p>;
   }
+}
   export default Part
